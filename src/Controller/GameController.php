e8776247ab13fd5e7b7d1e94aa2a7684e7531337
Sapozarom <36955178+Sapozarom@users.noreply.github.com>
2022-09-17 19:18:57 +0200

    vbasic game view

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,26 +2,35 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
+use App\Service\Calendar\Calendar;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
 {
-    #[Route('/game', name: 'app_game')]
-    public function index(): Response
-    {
-        return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
-        ]);
-    }
+    // #[Route('/game', name: 'app_game')]
+    // public function index(): Response
+    // {
+    //     return $this->render('game/index.html.twig', [
+    //         'controller_name' => 'GameController',
+    //     ]);
+    // }
 
     #[Route('/game/{game}', name: 'show_game')]
-    public function showGame(int $game): Response
+    public function showGame(int $game, GameRepository $gameRepo , Calendar $calendarService): Response
     {
+        $gameObj = $gameRepo->findOneBy(['id' => $game]);
 
+        $calendarArray =  $calendarService->setupCalendarByDate(new DateTime());
+        //dd($calendarArray);
+
+        
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+            'game' =>  $gameObj,
+            'calendarArray' => $calendarArray,
         ]);
     }
 }
--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -6,6 +6,7 @@ use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use Doctrine\ORM\EntityManagerInterface;
 
 class HomepageController extends AbstractController
 {
@@ -22,6 +23,7 @@ class HomepageController extends AbstractController
     #[Route('/games', name: 'app_homepage')]
     public function showGamesAction(GameRepository $gameRepo): Response
     {   
+        
         $user = $this->getUser();
         $uid = $user->getId();
         //dd($uid);
